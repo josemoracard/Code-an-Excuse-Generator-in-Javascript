aa6214cb843b7ce5a69ef6fdf4952ee368b4f793
josemoracard <jose.cardinales@gmail.com>
2023-06-06 23:00:03 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let div = document.getElementById("excuse");
+  div.innerHTML =
+    who[getRandomInt(4)] +
+    " " +
+    action[getRandomInt(4)] +
+    " " +
+    what[getRandomInt(3)] +
+    " " +
+    when[getRandomInt(5)];
 };
